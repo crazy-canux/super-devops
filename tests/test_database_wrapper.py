2936648f20cae0f59aab743a9c04e24d53fcdde6
Canux CHENG <WCheng@SonicWall.com>
2018-09-04 07:23:43 -0400

    update orm

--- tests/test_database_wrapper.py
+++ tests/test_database_wrapper.py
@@ -39,8 +39,8 @@ class ParamikoTestCase(unittest.TestCase):
 
     def test_dml_query(self):
         with BaseDB(
-            host='10.103.239.70', username='sandbox', password='P@ssword',
-            database='sandbox_60', port=1433
+                host='127.0.0.1', username='username',
+                password='password', database='test', port=1433
         ) as db:
             result = db.dml_query("delete from jobs where sample_sha256 = "
                                   "'4ed869e5c11e23218d808f91b341f4e5fc28f729c2477f71cee956cf50dd3b16'")
